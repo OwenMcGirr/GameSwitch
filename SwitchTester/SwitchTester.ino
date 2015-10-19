fd5ed9213381db27d4cf309794a00cf87e6441b5
OwenMcGirr <o.a.mcgirr@gmail.com>
2015-10-19 22:36:34 +0100

    Add SwitchTester sketch

---
+++ SwitchTester/SwitchTester.ino
@@ -0,0 +1,64 @@
+#define DEBOUNCE_DELAY 10
+
+// switch pins
+int inputSwitchA = 3;
+int inputSwitchB = 4;
+
+// led pin
+int arduinoLED = 13;
+
+// pin state variables
+boolean currentInputSwitchAState = LOW;
+boolean previousInputSwitchAState = LOW;
+boolean currentInputSwitchBState = LOW;
+boolean previousInputSwitchBState = LOW;
+
+void setup() {
+  // initialise IO
+  pinMode(inputSwitchA, INPUT);
+  pinMode(inputSwitchB, INPUT);
+  pinMode(arduinoLED, OUTPUT);
+}
+
+void loop() {
+  // debounce switches
+  debounceSwitches();
+
+  // if either switch is pressed, light led
+  if (currentInputSwitchAState == HIGH || currentInputSwitchBState == HIGH) {
+    digitalWrite(arduinoLED, HIGH);
+  }
+  else {
+    digitalWrite(arduinoLED, LOW);
+  }
+
+  // set previous input switch states
+  setPreviousSwitchStates();
+}
+
+
+/*
+ * Debounce functions
+ */
+
+boolean debounce(int pin, boolean previous) {
+  boolean current = digitalRead(pin);
+
+  if (current != previous) {
+    delay(DEBOUNCE_DELAY);
+    current = digitalRead(pin);
+  }
+
+  return current;
+}
+
+void debounceSwitches() {
+  currentInputSwitchAState = debounce(inputSwitchA, previousInputSwitchAState);
+  currentInputSwitchBState = debounce(inputSwitchB, previousInputSwitchBState);
+}
+
+void setPreviousSwitchStates() {
+  previousInputSwitchAState = currentInputSwitchAState;
+  previousInputSwitchBState = currentInputSwitchBState;
+}
+
