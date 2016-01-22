7de26080a2c90d1ec0136231953f7463149fa7c7
OwenMcGirr <o.a.mcgirr@gmail.com>
2016-01-22 16:21:44 +0000

    Add KeyboardKeyManager class (not used yet)

---
+++ GameSwitch/KeyboardKeyManager.h
@@ -0,0 +1,23 @@
+#ifndef KeyboardKeyManager_h
+#define KeyboardKeyManager_h
+
+#include "Arduino.h"
+
+#define DOWN_UP_DELAY 100
+
+class KeyboardKeyManager
+{
+  public:
+    KeyboardKeyManager();
+    void keyDownUp(char k);
+    void keyDown(char k);
+    void keyUp(char k);
+    void keyDownUp(int k);
+    void keyDown(int k);
+    void keyUp(int k);
+    void reset();
+  private:
+    // none
+};
+
+#endif
