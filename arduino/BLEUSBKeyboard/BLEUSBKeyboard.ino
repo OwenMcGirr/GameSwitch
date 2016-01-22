9e67bbcd24772333ba159d9bcbfe8da4fb85c8d2
OwenMcGirr <o.a.mcgirr@gmail.com>
2016-01-22 22:16:34 +0000

    Add basic BLE setup

--- arduino/BLEUSBKeyboard/BLEUSBKeyboard.ino
+++ arduino/BLEUSBKeyboard/BLEUSBKeyboard.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SPI.h>
+#include "Adafruit_BLE_UART.h"
 
+#define ADAFRUITBLE_REQ 10
+#define ADAFRUITBLE_RDY 2
+#define ADAFRUITBLE_RST 9
+
+Adafruit_BLE_UART uart = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
+
+void setup(void) {
+  // start serial
+  Serial.begin(9600);
+
+  // start uart
+  uart.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(void) {
 }
