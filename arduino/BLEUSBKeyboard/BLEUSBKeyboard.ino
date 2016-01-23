52759dc863ce49fb41f6f95ed854d436c6cc7243
OwenMcGirr <o.a.mcgirr@gmail.com>
2016-01-23 16:45:25 +0000

    Finish basic bluetooth setup

--- arduino/BLEUSBKeyboard/BLEUSBKeyboard.ino
+++ arduino/BLEUSBKeyboard/BLEUSBKeyboard.ino
@@ -1,18 +1,28 @@
 #include <SPI.h>
 #include "Adafruit_BLE_UART.h"
 
+// uart object parameters
 #define ADAFRUITBLE_REQ 10
 #define ADAFRUITBLE_RDY 2
 #define ADAFRUITBLE_RST 9
 
-Adafruit_BLE_UART uart = Adafruit_BLE_UART(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
+// uart object
+Adafruit_BLE_UART uart(ADAFRUITBLE_REQ, ADAFRUITBLE_RDY, ADAFRUITBLE_RST);
 
 void setup(void) {
+  // wait for serial
+  while (!Serial);
+
   // start serial
   Serial.begin(9600);
 
   // start uart
-  uart.begin();
+  if (!uart.begin()) {
+    Serial.println("Bluefruit not found!!!");
+  }
+  else {
+    Serial.println("Bluefruit found!!! UART started!!!");
+  }
 }
 
 void loop(void) {
