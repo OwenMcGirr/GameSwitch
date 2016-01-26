2e55a493c8f0f5c2f58f05bb592454d3aa58dcab
OwenMcGirr <o.a.mcgirr@gmail.com>
2016-01-26 22:09:45 +0000

    Disable custom keyboard support in GSKeys

--- iOS/GSKeys/GSKeys/AppDelegate.m
+++ iOS/GSKeys/GSKeys/AppDelegate.m
@@ -42,4 +42,9 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)application:(UIApplication *)application shouldAllowExtensionPointIdentifier:(NSString *)extensionPointIdentifier {
+    // Disallow custom keyboards
+    return ![extensionPointIdentifier isEqualToString:UIApplicationKeyboardExtensionPointIdentifier];
+}
+
 @end
