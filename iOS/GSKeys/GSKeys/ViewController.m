bb73645e565854648288e0cff011121b71c383f8
OwenMcGirr <o.a.mcgirr@gmail.com>
2016-01-27 14:32:44 +0000

    Add code to connect device to application

--- iOS/GSKeys/GSKeys/ViewController.m
+++ iOS/GSKeys/GSKeys/ViewController.m
@@ -7,14 +7,36 @@
 //
 
 #import "ViewController.h"
+#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface ViewController ()
+@interface ViewController ()<CBCentralManagerDelegate, CBPeripheralDelegate>
+
+@property (nonatomic) CBCentralManager *central;
+@property (nonatomic) CBPeripheral *peripheral;
+
+@property (nonatomic) CBUUID *uartServiceID;
+@property (nonatomic) CBService *uartService;
+@property (nonatomic) CBUUID *txCharacteristicID;
+@property (nonatomic) CBCharacteristic *txCharacteristic;
+@property (nonatomic) NSArray *desiredServiceIDs;
+@property (nonatomic) NSArray *desiredCharacteristicIDs;
 
 @end
 
 @implementation ViewController
 
 #pragma mark - view events
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // connect
+    self.central = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
+    self.uartServiceID = [CBUUID UUIDWithString:@"6e400001-b5a3-f393-e0a9-e50e24dcca9e"];
+    self.desiredServiceIDs = @[self.uartServiceID];
+    self.txCharacteristicID = [CBUUID UUIDWithString:@"6e400002-b5a3-f393-e0a9-e50e24dcca9e"];
+    self.desiredCharacteristicIDs = @[self.txCharacteristicID];
+}
 
 - (void)viewWillAppear:(BOOL)animated {
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
@@ -51,6 +73,44 @@
     }];
     
     [self updateViewConstraints];
+}
+
+
+#pragma mark - CBCentralManagerDelegate methods
+
+- (void)centralManagerDidUpdateState:(CBCentralManager *)central {
+    if (central.state == CBCentralManagerStatePoweredOn) {
+        [central scanForPeripheralsWithServices:self.desiredServiceIDs options:nil];
+    }
+}
+
+- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
+    self.peripheral = peripheral;
+    peripheral.delegate = self;
+    [central connectPeripheral:peripheral options:nil];
+}
+
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
+    [peripheral discoverServices:self.desiredServiceIDs];
+    NSLog(@"%@", peripheral.name);
+}
+
+
+#pragma mark - CBPeripheralDelegate methods
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error {
+    for (CBService *service in peripheral.services) {
+        self.uartService = service;
+        [peripheral discoverCharacteristics:self.desiredCharacteristicIDs forService:service];
+    }
+}
+
+- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error {
+    for (CBCharacteristic *characteristic in service.characteristics) {
+        NSLog(@"%@", characteristic);
+        self.txCharacteristic = characteristic;
+        [peripheral setNotifyValue:YES forCharacteristic:characteristic];
+    }
 }
 
 @end
