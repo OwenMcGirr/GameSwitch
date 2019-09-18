31fe09e69f04c26edb812024f8937427bdb843a4
OwenMcGirr <o.a.mcgirr@gmail.com>
2019-09-18 12:23:42 +0100

    Add xbox constants

--- arduino/GameSwitch/XboxManager.h
+++ arduino/GameSwitch/XboxManager.h
@@ -6,6 +6,19 @@
 
 #define DOWN_UP_DELAY 250
 
+#define AXIS_UP_LEFT -127
+#define AXIS_DOWN_RIGHT 127
+#define AXIS_MIDDLE 0
+
+#define X1_BUTTON 0
+#define X2_BUTTON 1
+#define LA_BUTTON 2
+#define LB_BUTTON 3
+#define A_BUTTON 4
+#define B_BUTTON 5
+#define VIEW_BUTTON 6
+#define MENU_BUTTON 7
+
 //Defining the joystick REPORT_ID and profile type
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
                    JOYSTICK_TYPE_JOYSTICK, 8, 0,
