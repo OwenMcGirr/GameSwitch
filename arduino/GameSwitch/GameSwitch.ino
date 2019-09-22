a068c92fadefe66bbca85caa9e206e9dc70d925a
OwenMcGirr <o.a.mcgirr@gmail.com>
2019-09-22 19:26:26 +0100

    Add BLE setup to sketch (left usb)

--- arduino/GameSwitch/GameSwitch.ino
+++ arduino/GameSwitch/GameSwitch.ino
@@ -1,9 +1,23 @@
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+#include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BTConfig.h"
+
 #include "timing.h"
 #include "modes.h"
 
 #include "InputSwitch.h"
 #include "RGBManager.h"
 #include "XboxManager.h"
+
+// ble object
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
 
 // input switches
 InputSwitch inputSwitchA;
@@ -44,6 +58,33 @@ void setup() {
 
   // start xbox
   xboxManager.begin();
+
+  // start ble
+  if (!ble.begin(true)) {
+    Serial.println("Bluefruit not found!!!");
+  }
+  else {
+    Serial.println("Bluefruit found!!! BLE started!!!");
+  }
+
+  // set broadcast name
+  if (!ble.sendCommandCheckOK(F("AT+GAPDEVNAME=GameSwitchLeftUSB"))) {
+    Serial.println("Could not set broadcast name!!!");
+  }
+
+  // print info
+  ble.info();
+
+  // disable verbose
+  ble.verbose(false);
+
+  // set mode
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  // wait for connection
+  while (!ble.isConnected()) {
+    delay(500);
+  }
 }
 
 void loop() {
