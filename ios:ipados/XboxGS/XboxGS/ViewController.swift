a03c2942b4c0ee52622fc8ae17028827365d9cd9
OwenMcGirr <o.a.mcgirr@gmail.com>
2019-09-26 14:29:45 +0100

    Add XboxGS project

---
+++ ios:ipados/XboxGS/XboxGS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XboxGS
+//
+//  Created by Owen McGirr on 26/09/2019.
+//  Copyright © 2019 Owen McGirr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
