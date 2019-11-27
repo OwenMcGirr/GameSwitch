1e426f47d43ff7b6cbb6cc4bef987f3ed1407700
OwenMcGirr <o.a.mcgirr@gmail.com>
2019-11-27 19:36:27 +0000

    Fix timing

--- firmware/GameSwitchLeftUSB/src/timing.h
+++ firmware/GameSwitchLeftUSB/src/timing.h
@@ -1,8 +1,8 @@
 // times for input switch holds in milliseconds
 #define SWITCH_HOLD_1 500
 #define SWITCH_HOLD_2 1000
-#define SWITCH_HOLD_3 1500
-#define SWITCH_HOLD_4 2000
+#define SWITCH_HOLD_3 2000
+#define SWITCH_HOLD_4 3000
 
 /*
  * input switch A can do different actions based on the number of times the user presses it
